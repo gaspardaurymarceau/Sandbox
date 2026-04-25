0128dd8d69527485e3c9ee62d3a883835894f68b
gaspardaurymarceau <gaspard.aurymarceau@gmail.com>
2026-04-25 08:05:09 +0200

    Fixed ./OCaml/knap, for which the -h option was not working

--- OCaml/IPF2/paire.ml
+++
@@ -1,7 +0,0 @@
-module Pair = struct
-    type ('a , 'b) t = 'a * 'b
-    let make (a : 'a) (b : 'b) : ('a , 'b) t = (a , b)
-    let first ((f , _) : ('a , 'b) t) : 'a = f
-    let second ((_ , s) : ('a , 'b) t) : 'b = s
-    let swap ((f , s) : ('a , 'b) t) : ('b , 'a) t = (b , a)
-end  
